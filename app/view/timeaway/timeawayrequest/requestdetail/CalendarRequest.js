eda2f430f58279904cdcead20f1f03a496973db2
Enrique Valverde <evalverde@alivebox.com>
2013-07-10 14:31:10 -0600

    Calendar Requests Centered

--- app/view/timeaway/timeawayrequest/requestdetail/CalendarRequest.js
+++ app/view/timeaway/timeawayrequest/requestdetail/CalendarRequest.js
@@ -5,70 +5,79 @@ Ext.define('Prototypes.view.timeaway.timeawayrequest.requestdetail.CalendarReque
     title: 'Calendar Request',
     height: '100%',
     width: '100%',
+    layout: {
+        type: 'vbox',
+        align: 'center'
+    },
     initComponent: function () {
 
         this.items = [
             {
                 xtype: 'container',
-                layout: 'column',
-                padding: '10 0 0 10',
-                items: [
+                items:[
                     {
-                        xtype: 'label',
-                        text: 'Request For:'
+                        xtype: 'container',
+                        layout: 'column',
+                        padding: '10 0 0 10',
+                        items: [
+                            {
+                                xtype: 'label',
+                                text: 'Request For:'
+                            },
+                            {
+                                xtype: 'label',
+                                itemId: 'lblRequest',
+                                padding: '0 0 0 10',
+                                style: 'font-weight:bold;color:#124faa;',
+                                text: 'Bruce Wayne'
+                            }
+                        ]
                     },
                     {
-                        xtype: 'label',
-                        itemId: 'lblRequest',
+                        xtype: 'container',
+                        layout: 'hbox',
+                        items: [
+                            {
+                                xtype: 'label',
+                                text: 'Category:',
+                                padding: '8 0 0 10'
+                            },
+                            {
+                                xtype: 'combobox',
+                                itemId: 'cbCategory',
+                                fieldLabel: null,
+                                displayField: 'name',
+                                valueField: 'name',
+                                padding: '5 0 10 28',
+                                store: Ext.create('Ext.data.Store', {
+                                    fields: ['id', 'name'],
+                                    data: [
+                                        {
+                                            "id": "1",
+                                            "name": "PTO"
+                                        },
+                                        {
+                                            "id": "2",
+                                            "name": "Training"
+                                        },
+                                        {
+                                            "id": "3",
+                                            "name": "Order"
+                                        }
+                                    ]
+                                }),
+                                editable: false,
+                                queryMode: 'local'
+                            }
+                        ]
+                    }
+                    ,
+                    {
+                        xtype: 'doubleenhanceddatepicker',
                         padding: '0 0 0 10',
-                        style: 'font-weight:bold;color:#124faa;',
-                        text: 'Bruce Wayne'
+                        itemId: 'doubleEnhancedDatePicker'
                     }
                 ]
-            },
-            {
-                xtype: 'container',
-                layout: 'hbox',
-                items: [
-                    {
-                        xtype: 'label',
-                        text: 'Category:',
-                        padding: '8 0 0 10'
-                    },
-                    {
-                        xtype: 'combobox',
-                        itemId: 'cbCategory',
-                        fieldLabel: null,
-                        displayField: 'name',
-                        valueField: 'name',
-                        padding: '5 0 10 28',
-                        store: Ext.create('Ext.data.Store', {
-                            fields: ['id', 'name'],
-                            data: [
-                                {
-                                    "id": "1",
-                                    "name": "PTO"
-                                },
-                                {
-                                    "id": "2",
-                                    "name": "Training"
-                                },
-                                {
-                                    "id": "3",
-                                    "name": "Order"
-                                }
-                            ]
-                        }),
-                        editable: false,
-                        queryMode: 'local'
-                    }
-                ]
-            }
-            ,
-            {
-                xtype: 'doubleenhanceddatepicker',
-                padding: '0 0 0 10',
-                itemId: 'doubleEnhancedDatePicker'
             }
         ]
         this.callParent(arguments)
